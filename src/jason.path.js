80f771e517c4f8a93b3c3c887be4e3ec99569cfb
Christian Bodart <chris@bodar.com>
2014-07-04 16:32:48 +0100

    introducing tracking

--- src/jason.path.js
+++ src/jason.path.js
@@ -4,6 +4,7 @@ function jason() { //handle recursion
 
 jason.Path = function() {
     this.functions = [];
+    this.tracking = [];
 };
 jason.Path.prototype = {
     execute: function jason_path_execute(object) { //overload?
@@ -14,22 +15,30 @@ jason.Path.prototype = {
         }
         return results;
     },
+    track: function (obj) {
+        var tracking = this.tracking;
+        if (tracking.indexOf(obj)!=-1) {
+            return true;
+        }
+        this.tracking.push(obj);
+        return false;
+    },
+    untrack: function () {
+        this.tracking.length = 0
+    },
     push: function() {
         [].push.apply(this.functions, arguments);
     },
-    results: function () {
-        var res = [];
-        res.push = function (o) {
-            this[this.length] = o;
-        }
-        res.concat = function () {
-            console.log("concat",arguments);
-            [].concat.apply(this,arguments)
-        }
-        return res
+    safePush: function () {
+        console.log("safePushing");
+        [].push.apply(this,arguments)
+    },
+    safeConcat: function () {
+         console.log("safeConcatting");
+        [].concat.apply(this,arguments);
     },
     filterObject: function jason_path_filterObject(object, filterFunction, filterCaller) {
-            var results = this.results();
+            var results = [];
             for (var i in object) {
                 if (object.hasOwnProperty(i)) {
                     if (filterFunction.call(this, object[i], i, object)) {
@@ -39,14 +48,68 @@ jason.Path.prototype = {
             }
             return results;
     },
-    each: function (object, eachFunction, filterCaller) {
-        for (var i in object) {
-            if (object.hasOwnProperty(i)) {
-                eachFunction.call(this, object[i], i, object);
+    each: function (object, eachFunction, track) {
+        var property;
+        if (object === null || object === void 0) {
+            return;
+        }
+        if (track) {
+            if (!this.track(object)) {
+                for (property in object) {
+                    if (object.hasOwnProperty(property)) {
+                        eachFunction.call(this,object[property], property, true)
+                    }
+                }
+            }
+            this.untrack();
+        }
+        else {
+            for (property in object) {
+                if (object.hasOwnProperty(property)) {
+                    eachFunction.call(this, object[property], property, object, false);
+                }
             }
         }
     },
-    isEnumerable: function jason_path_isEnumerable(value) {
+    isIn: function (property,value) {//safer in (doesn't error if we get passed nulls and undefined)
+        try {
+            return property in value;
+        }
+        catch (e) {
+            return false;
+        }
+        
+    },
+    isInstanceOf: function (value,type) {//safer instanceof (IE stuff really..)
+        return value instanceof type;
+    },
+    isPrimitive: function (value) {
+        return !(value instanceof Object);
+    },
+    isNative: function (value) {
+        if (typeof value == "function") {
+            return !(/^function\s+anonymous/.test(value));
+        }
+        if (this.isPrimitive(value)) {
+            return true;
+        }
+        if (value instanceof Array) {
+            return Object.prototype.toString.apply(value) == "[object Array]";
+        }
+        if (value.constructor == Object) {//in IE this can be hacked by setting the constructor of an object to Object manually...
+            console.warn("if youre supporting older versions of IE you cannot rely on this test...")
+            if (Object.getPrototypeOf && Object.getPrototypeOf(value) !== Object.prototype) {
+                return false;
+            }
+            return true;
+        }
+        return false;
+    },
+    isEnvironmental: function (value) {
+        if (this.isLiteral(value)) return false;
+        return /^function[^\{]+{\s*[native code]\s*}/mi.test(Function.prototype.toString.apply(value.constructor))
+    },
+    shouldEnumerate: function jason_path_isEnumerable(value) {
         if (typeof value == "object") {
             if (value instanceof Number) return false;
             if (value instanceof Boolean) return false;
@@ -55,15 +118,19 @@ jason.Path.prototype = {
         }
         return false;
     },
+    isCollection: function(value) {
+        return "length" in value && typeof value.length == "number"
+    },
     all: function jason_path_all() {
         this.push(function $jason_path_all(object) {
-            var results = this.results();
-            this.each(object, function $jason_path_all_filter(value) {             
-                if (this.isEnumerable(value)) {
-                    results.push(value);
-                    this.each(value, $jason_path_all_filter, $jason_path_all);
+            var results = [];
+            this.each(object, function $jason_path_all_filter(value) {    
+            results.push(value);         
+                if (this.shouldEnumerate(value)) {
+                    this.each(value, $jason_path_all_filter, true);
                 }
-            },$jason_path_all);
+            }, true);
+            this.untrack();
             return results;
         });
         return this;
@@ -72,15 +139,17 @@ jason.Path.prototype = {
         if (!property) {
             property = "";
         }
+
         this.push(function $jason_path_key(object) {
             var results = [];
-            this.filterObject(object, function jason_path_key_filter(value) {
-                if (this.isEnumerable(value)) {
+            this.each(object, function jason_path_key_filter(value) {
+
+                if (this.shouldEnumerate(value)) {
                     if (value.hasOwnProperty(property)) {
                         results.push(value[property]);
                     }
                 }
-            },$jason_path_key);
+            });
             return results;
         });
         return this;
@@ -89,14 +158,14 @@ jason.Path.prototype = {
         this.push(function $json_path_keys(object) {
             var results = [];
             this.filterObject(object, function json_path_keys_filter(value) {
-                if (this.isEnumerable(value)) {
+                if (this.shouldEnumerate(value)) {
                     for (var i = 0; i != keys.length; i++) {
                         if (value.hasOwnProperty(keys[i])) {
                             results.push(value[keys[i]]);
                         }
                     }
                 }
-            },$jason_path_keys);
+            });
             return results;
         });
         return this;
@@ -117,14 +186,14 @@ jason.Path.prototype = {
                     return false;
                 }
             });
-        },$json_path_function);
+        });
         return this;
     },
     property: function json_path_property(property) {
         this.push(function $json_path_property(object) {
             return this.filterObject(object, function $json_path_property_filter(value) {
                 return property in value;
-            },$json_path_property);
+            });
         });
         return this;
     },
@@ -132,9 +201,28 @@ jason.Path.prototype = {
         this.push(function $json_path_parent(object) {
             return this.filterObject(object, function $json_path_property_filter(value, key, context) {
                 return context;
-            },$json_path_parent);
+            });
         });
         return this;
     }
-
 };
+if (!this instanceof Object) {//IE no doubt
+    jason.Path.prototype.isPrimitive = function (value) {
+        return !(this.instanceOf(Object)) && typeof value != "object" && typeof value != "unknown";
+    };
+    jason.Path.prototype.isInstanceOf = function (value,type) {//safer instanceof (IE stuff really..)
+       try {
+            return value instanceof type
+       }
+       catch (e) {
+            if (value == null || value == void 0) {
+                return false;
+            }
+            if (type == Object && typeof value == "object" || typeof value == "unknown") {
+                return true;
+            }
+       }
+        return false;
+    };
+
+}
